75f47ed14b43cb941da39f042dff865688a01c71
kinngddx <ddxboss70@gmail.com>
2025-11-07 02:16:58 +0530

    Finally made it with no errors simple design

--- tweet/urls.py
+++ tweet/urls.py
@@ -13,6 +13,19 @@
 
 
 
+# from django.urls import path
+# from . import views
+
+# app_name = 'tweet'
+
+# urlpatterns = [
+#     path('', views.tweet_list, name='tweet_list'),
+#     path('create/', views.tweet_create, name='tweet_create'),
+#     path('<int:tweet_id>/edit/', views.tweet_edit, name='tweet_edit'),
+#     path('<int:tweet_id>/delete/', views.tweet_delete, name='tweet_delete'),
+# ]
+
+
 from django.urls import path
 from . import views
 
@@ -23,4 +36,6 @@ urlpatterns = [
     path('create/', views.tweet_create, name='tweet_create'),
     path('<int:tweet_id>/edit/', views.tweet_edit, name='tweet_edit'),
     path('<int:tweet_id>/delete/', views.tweet_delete, name='tweet_delete'),
+    path('register/', views.register, name='register'),  # ✅ Add this line
 ]
+
