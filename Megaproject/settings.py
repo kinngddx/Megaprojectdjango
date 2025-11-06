3d434aa6567c6d0f405ea9e17590f00148415489
kinngddx <ddxboss70@gmail.com>
2025-11-06 20:21:02 +0530

    Now i am facing the issue to run the server

--- Megaproject/settings.py
+++ Megaproject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Megaproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  #app ke ander templates rkh skta hu ab mai
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
